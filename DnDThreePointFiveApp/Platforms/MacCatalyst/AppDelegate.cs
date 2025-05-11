d450ca55fde06182295c42de4ef35032091831ac
EmpressOfTinyTrees <arthur.wiesthal@gmail.com>
2025-05-11 16:16:28 +0200

    Initial commit

---
+++ DnDThreePointFiveApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DnDThreePointFiveApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
